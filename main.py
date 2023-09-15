b5e4c3b56f9dd8c8d769d02e9e2199291d5f3777
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2023-09-15 23:06:21 +0300

    feat: add simple selenium implementation

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import time
+import datetime
+from selenium import webdriver as wd
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ranobeUrl = 'https://ranobehub.org/ranobe'
+testOutput = "test/testOutput.html"
+sourcePage = "test/sourcePage.html"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def writeInFile(filename, mode, data):
+    with open(filename, mode, encoding='utf-8') as file:
+        file.write(data + "\n")
+        file.close()
 
 
-# Press the green button in the gutter to run the script.
+def getSourceHtlm(url):
+    writeInFile(testOutput, "w", str(datetime.datetime.now()))
+    driver = wd.Firefox()
+    driver.maximize_window()
+
+    try:
+        driver.get(url=url)
+        time.sleep(3)
+
+        pages = driver.find_elements(By.CLASS_NAME, "page-item")
+        with open("test/page-items.html", "w") as file:
+            for page in pages:
+                file.write(str(page.tag_name) + "\n")
+
+            file.close()
+
+        action = ActionChains(driver)
+        action.click(pages[3]).perform()
+        time.sleep(10)
+        writeInFile(sourcePage, "a+", driver.page_source)
+    except Exception as ex:
+        print(ex)
+    finally:
+        writeInFile(testOutput, "a+", str(datetime.datetime.now()))
+        driver.close()
+        driver.quit()
+
+
+def main():
+    getSourceHtlm(ranobeUrl)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
