63c089b071bebcfc88f6fc119cfcf2fde0b1f5a5
EndlessCurry <screener09@gmail.com>
2023-09-20 00:03:10 +0400

    Запись списка новелл в отдельный файл

---
+++ src/Novel.py
@@ -0,0 +1,7 @@
+class Novel:
+
+    def __init__(self, title_rus: str, title_en: str, source_link: str, img_link: str):
+        self.title_rus = title_rus
+        self.title_en = title_en
+        self.source_link = source_link
+        self.img_link = img_link
--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 import os
 import time
-import datetime
+from bs4 import BeautifulSoup
 from selenium import webdriver as wd
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
+from Novel import Novel
 
 ranobe_url = 'https://ranobehub.org/ranobe'
-source_page = "sourcePage.html"
+source_page_file_name = "sourcePage.html"
+novel_list_file_name = "novel_list.txt"
 
 # Путь до текущего файла
 file_dir = os.path.dirname(os.path.realpath('__file__'))
 temp_data_dir = '../tempData/'
+
 
 def write_in_file(file_name: str, mode: str, data):
     file_name = os.path.join(file_dir, temp_data_dir + file_name)
@@ -19,6 +21,7 @@ def write_in_file(file_name: str, mode: str, data):
     with open(file_name, mode, encoding='utf-8') as file:
         file.write(data + "\n")
         file.close()
+
 
 def get_source_html(url):
     options = wd.FirefoxOptions()
@@ -39,12 +42,11 @@ def get_source_html(url):
             # Вычисляем новую высоту прокрутки и сравниваем с последней высотой прокрутки.
             new_height = driver.execute_script("return document.body.scrollHeight")
             if new_height == last_height:
-                write_in_file(source_page, "w", driver.page_source)
-                source_page_file = os.path.join(file_dir, temp_data_dir + source_page)
+                write_in_file(source_page_file_name, "w", driver.page_source)
+                source_page_file = os.path.join(file_dir, temp_data_dir + source_page_file_name)
                 os.path.abspath(os.path.realpath(source_page_file))
                 print("Прокрутка завершена")
                 break
-            last_height = new_height
             print("Появился новый контент, прокручиваем дальше")
     except Exception as ex:
         print(ex)
@@ -52,20 +54,36 @@ def get_source_html(url):
         driver.close()
         driver.quit()
 
+
 def main():
     get_source_html(ranobe_url)
-    source_page_file = os.path.join(file_dir, temp_data_dir + source_page)
+    source_page_file = os.path.join(file_dir, temp_data_dir + source_page_file_name)
     source_page_file = os.path.abspath(os.path.realpath(source_page_file))
+    novel_list_file = os.path.join(file_dir, temp_data_dir + novel_list_file_name)
+    novel_list_file = os.path.abspath(os.path.realpath(novel_list_file))
     with open(source_page_file, "r", encoding='utf-8') as file:
         soup = BeautifulSoup(file.read(), 'html.parser')
-        ranobe = soup.find_all("div", class_="eight wide mobile five wide tablet four wide computer column")
-        items: list[str] = []
-        for r in ranobe:
-            if r:
-                items.append(str(r))
-                print(r)
+        ranobe_div = soup.find_all("div", class_="eight wide mobile five wide tablet four wide computer column")
+        novel_list: list[Novel] = []
+        for ranobe in ranobe_div:
+            if ranobe:
+                novel_list.append(Novel(title_rus=ranobe.find('div', class_='header').find('h4').find('a').text.strip(),
+                                        title_en=ranobe.find('div', class_='header').find('h5').find('a').text.strip(),
+                                        source_link=ranobe.find('a', class_='image')['href'],
+                                        img_link=ranobe.find('img', class_='poster_grid')['data-src']))
+    with open(novel_list_file, "w", encoding='utf-8'):
+        for index, novel in enumerate(novel_list):
+            with open(novel_list_file, "a", encoding='utf-8') as file:
+                file.write(novel.title_rus + "\n")
+                file.write(novel.title_en + "\n")
+                file.write(novel.source_link + "\n")
+                file.write(novel.img_link + "\n")
+                file.close()
+            # print(f"Позиция {index + 1} {novel.title_rus}")
+            # print(f"Позиция {index + 1} {novel.title_en}")
+            # print(f"Позиция {index + 1} {novel.source_link}")
+            # print(f"Позиция {index + 1} {novel.img_link}")
 
-        print(str(len(items)))
 
 if __name__ == '__main__':
     main()
