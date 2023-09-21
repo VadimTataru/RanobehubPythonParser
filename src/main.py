313f45236591f5f4a9b5e0f84cf3f56ce026343c
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2023-09-21 20:28:22 +0300

    feat: сбор всех томов

--- src/main.py
+++ src/main.py
@@ -56,29 +56,47 @@ def get_source_html(url):
         driver.quit()
 
 
+def get_single_ranobe_source_html(url: str):
+    options = wd.FirefoxOptions()
+    options.add_argument("-headless")
+    driver = wd.Firefox(options)
+    driver.set_window_size(1920, 1080)
+    try:
+        driver.get(url)
+        time.sleep(3)
+        volumes = driver.find_elements(By.CLASS_NAME, 'contents-volume')
+        print(len(volumes))
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+
+
 def main():
+    get_single_ranobe_source_html('https://ranobehub.org/ranobe/932-the-great-mage-returns-after-4000-years')
     #get_source_html(ranobe_url)
-    source_page_file = os.path.join(file_dir, temp_data_dir + source_page_file_name)
-    source_page_file = os.path.abspath(os.path.realpath(source_page_file))
-    novel_list_file = os.path.join(file_dir, temp_data_dir + novel_list_file_name)
-    novel_list_file = os.path.abspath(os.path.realpath(novel_list_file))
-    with open(source_page_file, "r", encoding='utf-8') as file:
-        soup = BeautifulSoup(file.read(), 'html.parser')
-        ranobe_div = soup.find_all("div", class_="eight wide mobile five wide tablet four wide computer column")
-        novel_list: list[Novel] = []
-        for ranobe in ranobe_div:
-            if ranobe:
-                novel_list.append(Novel(title_rus=ranobe.find('div', class_='header').find('h4').find('a').text.strip(),
-                                        title_en=ranobe.find('div', class_='header').find('h5').find('a').text.strip(),
-                                        source_link=ranobe.find('a', class_='image')['href'],
-                                        img_link=ranobe.find('img', class_='poster_grid')['data-src']))
-    print(len(ranobe_div))
-    with open(novel_list_file, "w+", encoding='utf-8') as file:
-        file.write('[' + '\n')
-        for novel in novel_list:
-            file.write(json.dumps(novel.__dict__, indent=4, ensure_ascii=False))
-            file.write(', \n')
-        file.write(']')
+    # source_page_file = os.path.join(file_dir, temp_data_dir + source_page_file_name)
+    # source_page_file = os.path.abspath(os.path.realpath(source_page_file))
+    # novel_list_file = os.path.join(file_dir, temp_data_dir + novel_list_file_name)
+    # novel_list_file = os.path.abspath(os.path.realpath(novel_list_file))
+    # with open(source_page_file, "r", encoding='utf-8') as file:
+    #     soup = BeautifulSoup(file.read(), 'html.parser')
+    #     ranobe_div = soup.find_all("div", class_="eight wide mobile five wide tablet four wide computer column")
+    #     novel_list: list[Novel] = []
+    #     for ranobe in ranobe_div:
+    #         if ranobe:
+    #             novel_list.append(Novel(title_rus=ranobe.find('div', class_='header').find('h4').find('a').text.strip(),
+    #                                     title_en=ranobe.find('div', class_='header').find('h5').find('a').text.strip(),
+    #                                     source_link=ranobe.find('a', class_='image')['href'],
+    #                                     img_link=ranobe.find('img', class_='poster_grid')['data-src']))
+    # print(len(ranobe_div))
+    # with open(novel_list_file, "w+", encoding='utf-8') as file:
+    #     file.write('[' + '\n')
+    #     for novel in novel_list:
+    #         file.write(json.dumps(novel.__dict__, indent=4, ensure_ascii=False))
+    #         file.write(', \n')
+    #     file.write(']')
 
 
 if __name__ == '__main__':
