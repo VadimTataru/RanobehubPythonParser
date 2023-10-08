a0c24785d9eacf8a8282c13e790749368a0b2135
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2023-10-08 10:23:57 +0300

    feat: добавлены свойства в класс novelchapter.py

--- src/NovelChapter.py
+++ src/NovelChapter.py
@@ -3,8 +3,10 @@ from src.Novel import Novel
 
 class NovelChapter:
 
-    def __init__(self, title_rus: str, source_link: str, content: str, novel: Novel):
+    def __init__(self, title_rus: str, source_link: str, next_source_link: str, content: str, volume: str, novel: Novel):
         self.title_rus = title_rus
         self.source_link = source_link
+        self.next_source_link = next_source_link
         self.content = content
+        self.volume = volume
         self.novel = novel
